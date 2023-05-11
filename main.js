0bb47226762f141c66d33054861f44b2821bca49
Miwatora <114199218+Miwatora@users.noreply.github.com>
2023-05-11 16:56:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camrea');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-5Jzpj8KV/model.json', modelLoaded);
+
+function modelLoaded(){
+    console.log('modelLoaded');
+}
+
+function check()
+{
+    img = document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    
+    
+        }
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+  }
+
+
+
+
+
